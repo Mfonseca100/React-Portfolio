7710e47329e64e4754cf5e8615c94e6d1a3250aa
Edith <rissafonseca@yahoo.com>
2023-07-18 21:05:25 -0500

    fixed readme, still need to fix link to github

--- src/pages/ProjectDisplay.js
+++ src/pages/ProjectDisplay.js
@@ -3,10 +3,14 @@ import { useParams } from "react-router-dom";
 import { ProjectList } from "../helpers/ProjectList.js";
 import GitHubIcon from "@material-ui/icons/GitHub";
 import "../styles/Projects.css";
+import IconButton from "@material-ui/core/IconButton";
 
 function ProjectDisplay() {
     const { id } = useParams();
     const project = ProjectList[id];
+    const { name, image, skills, githubLink } = project;
+    
+    
     return (
         <div className="project">
             <h1> {project.name}</h1>
@@ -14,7 +18,14 @@ function ProjectDisplay() {
             <p>
                 <b>Skills:</b> {project.skills}
             </p>
+            <IconButton
+        component="a"
+        href={project.githubLink}
+        target="_blank"
+        rel="noopener noreferrer"
+      >
             <GitHubIcon />
+            </IconButton>
         </div>
     );
 }
